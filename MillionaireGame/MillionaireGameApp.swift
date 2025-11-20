6bef292ce3ad79eea7c6072b461c44ba775ad3f7
IncognitoCxde <benissaeaston@gmail.com>
2025-11-20 16:09:20 +0500

    Initial Commit

---
+++ MillionaireGame/MillionaireGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireGameApp.swift
+//  MillionaireGame
+//
+//  Created by iMacbook on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
