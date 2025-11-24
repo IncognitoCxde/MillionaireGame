572c5beaef8cadfd4b18a11da6a7a64354ca4446
IncognitoCxde <benissaeaston@gmail.com>
2025-11-24 14:03:05 +0500

    Added .DS_Store to gitignore

---
+++ MillionaireGame/SplashView.swift
@@ -0,0 +1,19 @@
+// Content View B Manuchehri
+
+import SwiftUI
+import DesignSystem
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
